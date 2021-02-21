8c34c6e667fc0d97e79e6190bdaab2b924efd086
Punit Jain <contactpunitjain@gmail.com>
2021-02-21 22:45:07 +0530

    added mongodb string to test connectivity with Atlas

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongodb = require('mongodb').MongoClient;
 
 const productRoutes = require('./routes/products');
 const authRoutes = require('./routes/auth');
@@ -24,5 +25,14 @@ app.use((req, res, next) => {
 
 app.use('/products', productRoutes);
 app.use('/', authRoutes);
+
+mongodb.connect('mongodb+srv://dbuser:TdpUurGYjP3dfTOv@sonic.tsoj2.mongodb.net/shop?retryWrites=true&w=majority')
+  .then((client) => {
+    console.log('Connected');
+    client.close();
+  })
+  .catch(err => {
+    console.log(err)
+  })
 
 app.listen(3100);
